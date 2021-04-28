37c6a381eb43278625623d6259f68b5954020436
Matt <matt.collins087@gmail.com>
2021-04-28 07:49:50 -0500

    isPowerOfThree Completed

---
+++ isPowerOfThree.js
@@ -0,0 +1,34 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// Example 1:
+
+// Input: n = 27
+// Output: true
+// Example 2:
+
+// Input: n = 0
+// Output: false
+// Example 3:
+
+// Input: n = 9
+// Output: true
+
+
+const isPowerOfThree = (n) => {
+  const recurse = (num) => {
+    if (n === 1) {
+      return true;
+    }
+
+    if (n < 1) {
+      return false;
+    }
+
+    return isPowerOfThree(n / 3);
+  }
+
+  return recurse(n);
+}
+
